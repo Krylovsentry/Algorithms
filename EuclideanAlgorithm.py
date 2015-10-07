7f17df706e6ae774665260ceed62ac1440d358a8
Anton <krylovsentry@yandex.ru>
2015-10-07 15:32:11 +0300

    Test commit from PyCharm

--- EuclideanAlgorithm.py
+++ EuclideanAlgorithm.py
@@ -1,1 +1,16 @@
 
+
+#Efficient method for computing the greatest common divisor (GCD) of two numbers
+def gcd(a,b):
+
+    while a % b != 0 :
+
+        den = a % b
+        a = b
+        b = den
+
+    return b
+
+print(gcd(125,55))
+print(gcd(46,78))
+
