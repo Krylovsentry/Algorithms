c55b6f2be2768613b63e6d48318c54dae04977ee
Anton <krylovsentry@yandex.ru>
2017-04-07 16:42:31 +0300

    format some scripts

--- EuclideanAlgorithm.py
+++ EuclideanAlgorithm.py
@@ -1,16 +1,12 @@
-
-
-#Efficient method for computing the greatest common divisor (GCD) of two numbers
-def gcd(a,b):
-
-    while a % b != 0 :
-
+# Efficient method for computing the greatest common divisor (GCD) of two numbers
+def gcd(a, b):
+    while a % b != 0:
         den = a % b
         a = b
         b = den
 
     return b
 
-print(gcd(125,55))
-print(gcd(46,78))
 
+print(gcd(125, 55))
+print(gcd(46, 78))
--- Fibonacci.py
+++ Fibonacci.py
@@ -2,27 +2,28 @@
 Fibonacci numbers
 """
 
-#Difficult growths by exp
-def fib_recursion(n):
 
+# Difficult growths by exp
+def fib_recursion(n):
     if n < 2:
         return n
 
-    return fib_recursion(n-1) + fib_recursion(n-2)
+    return fib_recursion(n - 1) + fib_recursion(n - 2)
 
 
 print(fib_recursion(1))
 
+
 # Difficult O(n)
 def fibonacci(n):
-
-    number = [x for x in range(n+1)]
+    number = [x for x in range(n + 1)]
     number[0] = 0
     number[1] = 1
 
-    for i in range(2,n+1):
-        number[i] = number[i-1] + number[i-2]
+    for i in range(2, n + 1):
+        number[i] = number[i - 1] + number[i - 2]
 
     return number[n]
+
 
 print(fibonacci(2222))
